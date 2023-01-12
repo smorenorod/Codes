28d2e2378e83a4a63251c04a9f384ad8492b2311
Sebastian Moreno R <122574852+smorenorod@users.noreply.github.com>
2023-01-12 17:45:54 -0500

    Update Sensor Tower API-Copy1.py

--- Sensor Tower API-Copy1.py
+++ Sensor Tower API-Copy1.py
@@ -28,13 +28,13 @@ import json
 
 
 # In[151]:
-
+#you should generate an API token in sensor tower and add it to this url
 
 APP_ANDROID_INFO = 'https://api.sensortower.com/v1/android/sales_report_estimates?app_ids=com.dreamgames.royalmatch                    %2Ccom.gamegos.mobile.manorcafe%2Ccom.matchington.                    mansion&countries=AE%2CAO%2CAR%2CAT%2CAU%2CAZ%2CBB%2CBE%2CBG%2CBH%2CBM%2CBO%2CBR%2CBY%2CCA%2CCH%2CCL%2CCO%2CCR%2CCY%2CCZ%2CDE%2CDK%2CDO%2CDZ%2CEC%2CEE%2CEG%2CES%2CFI%2CFR%2CGB%2CGE%2CGH%2CGR%2CGT%2CHK%2CHR%2CHU%2CID%2CIE%2CIL%2CIN%2CIT%2CJP%2CKE%2CKH%2CKR%2CKW%2CKZ%2CLB%2CLK%2CLT%2CLU%2CLV%2CMG%2CMO%2CMT%2CMX%2CMY%2CNG%2CNI%2CNL%2CNO%2CNZ%2COM%2CPA%2CPE%2CPH%2CPK%2CPL%2CPT%2CPY%2CQA%2CRO%2CRS%2CRU%2CSA%2CSE%2CSG%2CSI%2CSK%2CSV%2CTH%2CTN%2CTR%2CTW%2CUA%2CUS%2CUY%2CUZ%2CVE%2CVN%2CZA&date                    _granularity=daily&start_date=2023-01-05&end_date=2023-01-11&auth_token=here_goes_your_api_token'
 
 
 # In[236]:
-
+#you should generate an API token in sensor tower and add it to this url
 
 APP_IOS_INFO ='https://api.sensortower.com/v1/ios/sales_report_estimates?app_ids=                1195621598%2C1216575026%2C1228655110%2C1358742711%2C%201482155847%2C%201570403223%2C&countries                =AE%2CAO%2CAR%2CAT%2CAU%2CAZ%2CBB%2CBE%2CBG%2CBM%2CBO%2CBR%2CBY%2CCA%2CCH%2CCL%2CCN%2CCO%2CCR%2CCZ%2CDE%2CDK%2CDO%2CDZ%2CEC%2CEE%2CEG%2CES%2CFI%2CFR%2CGB%2CGH%2CGR%2CGT%2CHK%2CHR%2CHU%2CID%2CIE%2CIL%2CIN%2CIT%2CJP%2CKE%2CKH%2CKR%2CKW%2CKZ%2CLB%2CLK%2CLT%2CLU%2CLV%2CMG%2CMO%2CMX%2CMY%2CNG%2CNI%2CNL%2CNO%2CNZ%2COM%2CPA%2CPE%2CPH%2CPK%2CPL%2CPT%2CPY%2CQA%2CRO%2CRU%2CSA%2CSE%2CSG%2CSI%2CSK%2CSV%2CTH%2CTN%2CTR%2CTW%2CUA%2CUS%2CUY%2CUZ%2CVE%2CVN%2CZA%2CBH%2CCY%2CGE%2CMT%2CRS%2CWW%2C&date                _granularity=daily&start_date=2023-01-05&end_date=2023-01-11&auth_token=here_goes_your_api_token'
 
